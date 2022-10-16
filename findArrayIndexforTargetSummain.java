279e427c9db6deb041b5095ece5b2b749f4c6c41
Arthi TV <60561674+arthi-tv-sdet@users.noreply.github.com>
2022-10-16 06:48:04 +0530

    Update and rename findArrayIndexforTargetSum.java to findArrayIndexforTargetSummain.java
    finaArrayIndexforTargetSummain- with only method method

--- findArrayIndexforTargetSummain.java
+++ findArrayIndexforTargetSummain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
-public class findArrayIndexforTargetSum {
+public class findArrayIndexforTargetSummain {
 
 	public static void main(String args[]) {
 		
