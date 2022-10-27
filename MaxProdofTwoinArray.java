1b84bc9aab869f5debd1a6c84e1472d6ea51405d
Arthi TV <60561674+arthi-tv-sdet@users.noreply.github.com>
2022-10-27 08:06:20 +0530

    2 pointers

--- MaxProdofTwoinArray.java
+++ MaxProdofTwoinArray.java
@@ -1,4 +1,4 @@
-package DSA;
+package DSA_Array;
 
 import java.util.Arrays;
 
@@ -9,12 +9,11 @@ public class MaxProdofTwoinArray {
 	/*
 	 * 1464. Maximum Product of Two Elements in an Array
 	 * 
-	 * Pseudocode:
-	 * 
+	 *  
 	 * Input: nums = [3,4,5,2]
 		Output: 12 
 	 * 
-	 * 
+	 * Pseudocode (brute force):
 	 * sort the given array
 	 * i=num[length-1] j=num[length-2]
 	 * return the value of (i-1)*(j-1)
@@ -27,10 +26,15 @@ public class MaxProdofTwoinArray {
 	 * Edge: {5,5,5,5}---> output: 16
 	 * 
 	 * 
+	 * Pseudocode: 2 pointers
+	 * initialize 2 pointers start=0,end=1, maxProd=Integer.MIN_VALUE,product=1
+	 * decrement 1 to value of start and end and find the product
+	 * compare maxProd and product for each iteration and return the maxProd
+	 * 
 	*/
 	
 	
-	private void MaxProdofTwoinArray(int nums[])
+	private void MaxProdofTwoinArray_BF(int nums[])
 	{
 		if(nums.length!=0) {
 			Arrays.sort(nums);
@@ -41,26 +45,54 @@ public class MaxProdofTwoinArray {
 			System.out.println("Empty array -could not find Max product");
 	}
 	
-	
+	private void MaxProdofTwoinArray_2P(int nums[])
+	{
+		int start=0,end=1,maxProd=Integer.MIN_VALUE,product=1,len=nums.length-1;
+		
+		if(nums.length!=0)
+		{
+		while(start<end && end <len && start<len)
+			
+		{
+			product=nums[start]-- * nums[end]--;
+			maxProd=Math.max(maxProd, product);
+			start++; end++;
+		}
+		if(!(maxProd<=0))
+		System.out.println("output : "+maxProd);
+		else
+			System.out.println("output : "+product);
+		}
+		
+			else
+		System.out.println("Empty array -could not find Max product");
+				
+				
+	}
+
+
 	
 	@Test
 	public void MaxProdNegative()
 	{
 		int nums[]={};
-		MaxProdofTwoinArray(nums);
+		///MaxProdofTwoinArray_BF(nums);
+		MaxProdofTwoinArray_2P(nums);
 	}
 	
 	@Test
 	public void MaxProdEdge()
 	{
 		int nums[]={-1,-1};
-		MaxProdofTwoinArray(nums);
+	//	MaxProdofTwoinArray_BF(nums);
+		MaxProdofTwoinArray_2P(nums);
 	}
 	
 	@Test
 	public void MaxProdPositive()
 {
 	int nums[]={3,4,5,2};
-	MaxProdofTwoinArray(nums);
+	//MaxProdofTwoinArray_BF(nums);
+	MaxProdofTwoinArray_2P(nums);
 }
 }
