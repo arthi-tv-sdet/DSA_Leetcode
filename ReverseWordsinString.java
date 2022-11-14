0e6a809db80ec531d783f03f224299a6402a32a8
Arthi TV <60561674+arthi-tv-sdet@users.noreply.github.com>
2022-11-14 07:02:40 +0530

    Optimised- 2 pointer

--- ReverseWordsinString.java
+++ ReverseWordsinString.java
@@ -15,7 +15,7 @@ public class ReverseWordsinString {
 		Input: s = "Let's take LeetCode contest"
 		Output: "s'teL ekat edoCteeL tsetnoc"	
 	 * 
-	 * 
+	 * Pseudocode- Using String buffer
 	 * convert string to stringbuffer and reverse
 	 * store the reversed stringbuffer value in a string
 	 * split the string into array of strings using ""
@@ -23,8 +23,13 @@ public class ReverseWordsinString {
 	 * print each string from array in reverse 
 	 * 	
 	 * 
-	 * 
-	 * 
+	 * Pseudocode- Using 2 Pointer (Optimised)
+	 * split the sentence to string array
+	 * traverse through each word of array
+	 *in a separate method reverse the charaters of each word using 2 pointers left and right , temp to swap
+	 *increment left, decrement right after each swap
+	 *reversed string is appended to temp string
+	 *finally trim temp and return
 	*/
 	
 	private String reverseWords(String in)
@@ -42,17 +47,46 @@ public class ReverseWordsinString {
 	System.out.print(finalResult);
 	return finalResult.trim();
 }
-	
+	  public String reverseWords_2P(String s) {
+        
+        String arr[]=s.split(" ");
+        String temp="";
+       
+        for(int i=0;i<arr.length;i++)
+        {
+          int len=arr[i].length()-1;  
+            temp=temp+reverse(arr[i],0,len)+" ";
+        }
+        
+        return temp.trim();
+    }
+    
+    public String reverse(String temp, int left,int right)
+    {
+        char arr[]=temp.toCharArray();
+        
+        while(left<right)
+        {
+            char t=arr[left];
+            arr[left++]=arr[right];
+            arr[right--]=t;
+          //  right--;
+          //  left++;
+        }
+        return String.valueOf(arr);
+    }
 	@Test
 	public void reverseWordsInput1()
 	{
 		String in="Let's take LeetCode contest";
 		reverseWords(in);
+		reverseWords_2P(in);
 	}
 	@Test
 	public void reverseWordsInput2()
 	{
 		String in="God Ding";
 		reverseWords(in);
+		reverseWords_2P(in);
 	}
 }
