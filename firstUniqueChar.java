a3cae061158bdfedfd2b5d1d461886c25f35f026
Arthi TV <60561674+arthi-tv-sdet@users.noreply.github.com>
2022-11-14 07:13:37 +0530

    Updated

--- firstUniqueChar.java
+++ firstUniqueChar.java
@@ -44,6 +44,14 @@ public class FirstUniqueCharacter {
 	 
 	  Time: O[N]
 	 Space: O[N]
+	 
+	 * Pseudocode: using HashMap
+	 * initialize a hashmap , traverse through string- add each charAt of s and occurence in key and value respectively
+	 * traverse through the string if the value for the charAt of s is 1 return the key
+	 
+	 Time: O[N]
+	 Space: O[N]
+	 
 	 */
 	
 	@Test
@@ -69,6 +77,17 @@ public class FirstUniqueCharacter {
 		firstUniqueCharArray(s);
 		//firstUniqueChar(s);
 	}
+	
+	/ * Pseudocode: Brute force
+	 * traverse through the string
+	 * compare each charAt(i) with chatAt(j)
+	 * if both index has same character increment temp
+	 * return the index if temp==1
+	 * else return -1
+	 * 
+	  Time: O[N]
+	 Space: O[1] */
+		 
 	private int firstUniqueChar_BF(String s) {
 		
 		int temp=0;
@@ -96,6 +115,19 @@ public class FirstUniqueCharacter {
 		System.out.println("-1");
 		return -1;
 	}
+	
+	 /* Pseudocode: using array
+	 * initialize a character array of length 26
+	 * traverse through the string
+	 * for each char At (i) increment the corresponding value of char position in char Array
+	 * find the index of char with value 1
+	 * else return -1
+	 
+	  Time: O[N]
+	 Space: O[N]
+	*/
+	
+	
 	 private int firstUniqueCharArray(String s)
 	 {
 		int arr[]=new int[26];
@@ -115,6 +147,16 @@ public class FirstUniqueCharacter {
 		return -1;
 	 }
 	
+	/ * Pseudocode:String method
+	 * traverse through the string
+	 * compare the index using below methods
+	 * string methods indexOf and lastIndexOf
+	 * if both index are same return the index else return -1
+	 
+	 Time: O[N]
+	 Space: O[1]
+	 */ 
+	
 	 private int firstUniqueChar(String s)
 	 {
 		for(int i=0;i<s.length();i++)
@@ -129,4 +171,30 @@ public class FirstUniqueCharacter {
 		return -1;
 		
 	 }
+	
+	/ * Pseudocode: using HashMap
+	 * initialize a hashmap , traverse through string- add each charAt of s and occurence in key and value respectively
+	 * traverse through the string if the value for the charAt of s is 1 return the key
+	 
+	* Time: O[N]
+	* Space: O[N]
+		 */
+		
+	 public int firstUniqChar(String s) {
+  HashMap <Character,Integer> hmap=new HashMap<>();
+
+for(int i=0;i<s.length();i++)
+{
+    hmap.put(s.charAt(i),hmap.getOrDefault(s.charAt(i),0)+1);
+}
+
+for(int i=0;i<s.length();i++)
+{
+    if(hmap.get(s.charAt(i))==1)
+    return i;
+
+}
+		return -1;
+    
+}
 }
